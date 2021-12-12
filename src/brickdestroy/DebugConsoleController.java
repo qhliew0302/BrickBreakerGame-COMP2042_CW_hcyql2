d9a2d6b2bc30801b94a7cf6854a8db10dff17ee7
Qian Hui <qhliew0302@gmail.com>
2021-12-12 20:07:46 +0800

    Implement MVC to Debug Console class

---
+++ src/brickdestroy/DebugConsoleController.java
@@ -0,0 +1,49 @@
+package brickdestroy;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class DebugConsoleController implements WindowListener {
+    private DebugConsole debugConsole;
+
+    public DebugConsoleController(DebugConsole debugConsole){
+        this.debugConsole = debugConsole;
+
+    }
+    @Override
+    public void windowOpened(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent windowEvent) {
+        debugConsole.getGameBoard().repaint();
+    }
+
+    @Override
+    public void windowClosed(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent windowEvent) {
+        debugConsole.setLocation();
+        Ball b = debugConsole.getWall().getBall();
+        debugConsole.getDebugPanel().setValues(b.getSpeedX(),b.getSpeedY());
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent windowEvent) {
+
+    }
+}
