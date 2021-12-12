00233076932e66c1c86589c89c840f78c08adfce
Qian Hui <qhliew0302@gmail.com>
2021-12-13 02:40:21 +0800

    Create javadoc for cement brick class

--- src/main/java/brickdestroy/model/CementBrick.java
+++ src/main/java/brickdestroy/model/CementBrick.java
@@ -4,7 +4,9 @@ import java.awt.*;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 
-
+/**
+ * A cement brick class that inherits abstract brick class.
+ */
 public class CementBrick extends Brick {
 
 
@@ -20,15 +22,27 @@ public class CementBrick extends Brick {
     private Shape brickFace;
 
 
+    /**
+     * A cement brick constructor that initialises the cement brick variables.
+     *
+     * @param point position of cement brick
+     * @param size size of cement brick
+     */
     public CementBrick(Point point, Dimension size){
         super(NAME,point,size,DEF_BORDER,DEF_INNER,CEMENT_STRENGTH);
         crack = new Crack(this,DEF_CRACK_DEPTH,DEF_STEPS);
         brickFace = super.getBrickFace();
     }
 
+    /**
+     * if the brick is broken, no impact is set
+     * else make an impact and if the brick is not broken, then make a crack
+     *
+     * @param point position of the brick
+     * @param dir direction of impact of brick
+     * @return cement brick status
+     */
 
-    // if the brick is broken, no impact is set
-    // else make an impact and if the brick is not broken, then make a crack
     @Override
     public boolean setImpact(Point2D point, int dir) {
         if(super.isBroken())
@@ -43,12 +57,19 @@ public class CementBrick extends Brick {
     }
 
 
+    /**
+     * Gets the cement brick shape.
+     *
+     * @return cement brick shape
+     */
     @Override
     public Shape getBrick() {
         return brickFace;
     }
 
-    // update brick if the brick is not broken and draw a crack on it
+    /**
+     * Updates brick if the brick is not broken and draw a crack on it.
+     */
     private void updateBrick(){
         if(!super.isBroken()){
             GeneralPath gp = crack.draw();
@@ -57,7 +78,9 @@ public class CementBrick extends Brick {
         }
     }
 
-    // repair the brick by removing the crack
+    /**
+     * Repairs the cement brick by removing the crack.
+     */
     public void repair(){
         super.repair();
         crack.reset();
